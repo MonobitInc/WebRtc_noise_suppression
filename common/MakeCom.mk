f5f42cb78b911464ff4501d6b9bf72384c7d6fb0
vacingfang <vacingfang@tencent.com>
2019-11-18 09:44:28 +0800

    add webrtc directory to common, update AudioFile library

---
+++ common/MakeCom.mk
@@ -0,0 +1,21 @@
+
+WEBRTC_ROOT = ${COMMON_ROOT}/webrtc
+
+INCS += -I ${COMMON_ROOT}
+
+INCS += -I ${WEBRTC_ROOT}
+WEBRTC_CCS += $(wildcard ${WEBRTC_ROOT}/common_audio/resampler/*.cc)
+WEBRTC_CCS += $(wildcard ${WEBRTC_ROOT}/api/units/*.cc)
+WEBRTC_CCS += $(wildcard ${WEBRTC_ROOT}/rtc_base/*.cc)
+WEBRTC_CCS += $(wildcard ${WEBRTC_ROOT}/rtc_base/memory/*.cc)
+WEBRTC_CCS += $(wildcard ${WEBRTC_ROOT}/modules/audio_processing/*.cc)
+WEBRTC_CCS += $(wildcard ${WEBRTC_ROOT}/modules/audio_processing/ns/*.cc)
+WEBRTC_OBJS += $(WEBRTC_CCS:.cc=.o)
+WEBRTC_CS += $(wildcard ${WEBRTC_ROOT}/modules/audio_processing/legacy_ns/*.c)
+WEBRTC_CS += $(wildcard ${WEBRTC_ROOT}/common_audio/third_party/fft4g/*.c)
+WEBRTC_CS += $(wildcard ${WEBRTC_ROOT}/common_audio/third_party/spl_sqrt_floor/*.c)
+WEBRTC_OBJS += $(WEBRTC_CS:.c=.o)
+
+libwebrtc.a:${WEBRTC_OBJS}
+	$(AR) -r $@ $^
+
